3a7009917406ccc2edc17d11a7fcb14f7a085f22
daashandayo <dj30c.1@gmail.com>
2019-07-05 21:33:14 +0530

    Add Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,24 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_and_belongs_to_many :friendships,
+      class_name: "User", 
+      join_table:  :friendships, 
+      foreign_key: :user_id, 
+      association_foreign_key: :friend_id
+  
+  has_and_belongs_to_many :inverse_friendships,
+      class_name: "User", 
+      join_table:  :friendships, 
+      foreign_key: :friend_id, 
+      association_foreign_key: :user_id
+
+
+
+  def add_friend(friend_id)
+    Friendship.create(user_id: self.id, friend_id: friend_id)
+    Friendship.create(user_id: friend_id, friend_id: self.id)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_133418) do
+ActiveRecord::Schema.define(version: 2019_07_05_151035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2019_07_05_133418) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id", "user_id"], name: "index_friendships_on_friend_id_and_user_id", unique: true
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
