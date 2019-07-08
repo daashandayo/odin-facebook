633123ff96986d0e4992c1250c170b09918723d4
bellom <bellomsean@gmail.com>
2019-07-08 11:38:00 +0100

    added rspec with shoulda gem

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:likes). dependent(:destroy) }
+    it { should have_many(:comments). dependent(:destroy) }
+  end
+end
