2dc531ae73a6a1782fc803ccbbb56c0ed2d35ae1
stickler-ci <support@stickler-ci.com>
2019-07-08 11:11:21 +0000

    Fixing style errors.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "associations" do
+  context 'associations' do
     it { should have_many(:posts). dependent(:destroy) }
     it { should have_many(:comments). dependent(:destroy) }
     it { should have_many(:likes). dependent(:destroy) }
