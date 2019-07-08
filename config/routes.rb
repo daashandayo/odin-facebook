0ad749e48db936fa71058e2e15bac1dd53eb3f50
daashandayo <dj30c.1@gmail.com>
2019-07-08 20:51:41 +0530

    Authenticate users before accessing site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/users', to: "users#index"
+  get 'users/show/:id', to: "users#show", as: "users_show"
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: "home#index"
