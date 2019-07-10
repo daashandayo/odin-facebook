ad05ebea7e0470111490ad39a74fd2124bcbcf16
daashandayo <dj30c.1@gmail.com>
2019-07-10 20:49:19 +0530

    Add facebook login feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get '/users', to: "users#index"
   get 'users/show/:id', to: "users#show", as: "users_show"
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: "home#index"
   post '/friendships', to: "friendships#create", as: "friendships_create"
