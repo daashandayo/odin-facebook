9757bd1ffd304f50df1d848223fb6dc393fcbd95
daashandayo <dj30c.1@gmail.com>
2019-07-16 17:45:00 +0530

    Add model tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -8,3 +8,26 @@ RSpec.describe Like, type: :model do
     it { should belong_to(:post) }
   end
 end
+
+describe Like do
+  it "has valid Factory" do
+    expect(FactoryBot.build(:like)).to be_valid
+  end
+
+  it "is invalid without Liked post" do
+    expect(FactoryBot.build(:like, post_id: nil)).not_to be_valid
+  end
+
+  it "is invalid without Liked user" do
+    expect(FactoryBot.build(:post, user_id: nil)).not_to be_valid
+  end
+
+  it "does not allow same user to like a post multiple times" do
+    liker = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
+    # Like.create(user_id: liker.id, post_id: post.id)
+    FactoryBot.create(:like, user_id: liker.id, post_id: post.id)
+    expect(FactoryBot.build(:like, user_id: liker.id, post_id: post.id)).not_to be_valid
+  end
+end
+
